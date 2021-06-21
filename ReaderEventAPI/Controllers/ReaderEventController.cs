a622c8392eb61678b31df04587a5012e116aa9dc
Michael Dacar <michaeldacar@hotmail.com>
2021-06-21 13:44:53 -0400

    Initial Code Commit

---
+++ ReaderEventAPI/Controllers/ReaderEventController.cs
@@ -0,0 +1,45 @@
+﻿using Confluent.Kafka;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace ReaderEventAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReaderEventController : ControllerBase
+    {
+
+
+        // POST api/<ReaderEvent>
+        [HttpPost]
+        public void Post([FromBody] TimeEntry value)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "pkc-epwny.eastus.azure.confluent.cloud:9092",
+                ClientId = Dns.GetHostName(),
+                SaslMechanism = SaslMechanism.Plain,
+                SecurityProtocol = SecurityProtocol.SaslSsl,
+                SaslUsername = "DL7ZEA3FYCX4RS3Z",
+                SaslPassword = "sYtS2HnvacXEwVh6thsvujyKCdZbfrKaGRSKWYyzoFD9jD6OhlA+0fDy+Fifef7j",
+            };
+
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                producer.Produce("time_entries", new Message<Null, string> { Value = value.ToString() }, DeliveryHandler);
+                producer.Flush(TimeSpan.FromSeconds(10));
+            }
+        }
+
+        private void DeliveryHandler(DeliveryReport<Null, string> report)
+        {
+            Console.WriteLine(report.Error.Reason);
+        }
+
+    }
+}
---
+++ ReaderEventAPI/TimeEntry.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+
+
+namespace ReaderEventAPI
+{
+    public class TimeEntry
+    {
+        public string ReaderName { get; set; }
+        public string UnixTime { get; set; }
+        public int AntennaNumber { get; set; }
+        public string TagId { get; set; }
+
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
