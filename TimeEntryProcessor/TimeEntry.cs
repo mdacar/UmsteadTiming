5b9ea15a2b6c06417003a575aeadbd0a3ef6c7df
mdacar <michaeldacar@hotmail.com>
2021-07-19 19:05:35 -0400

    Added the new time entry  processor project

---
+++ TimeEntryProcessor/TimeEntry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeEntryProcessor
+{
+    public class TimeEntry
+    {
+        public Guid ID { get; set; }
+        public Guid RaceXRunnerID { get; set; }
+        public long ElapsedTime { get; set; }
+        public DateTime AbsoluteTime { get; set; }
+        public long ReaderTimestamp { get; set; }
+        public int RFIDReaderID { get; set; }
+        public int TimeEntryStatusID { get; set; }
+        public int? TimeEntrySource { get; set; }
+        public string TagType { get; set; }
+    }
+}
